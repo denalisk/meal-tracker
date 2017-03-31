6b81d61905b528e32dbde8557ecf4fa0079a3321
Sam Kirsch <samdkirsch@gmail.com>
2017-03-31 08:51:10 -0700

    flesh out basic structure of MealListComponent

---
+++ app/meal-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Meal } from './models/meal.model';
+
+@Component({
+  selector: 'meal-list',
+  template: `
+  <div class="well">
+    <h1>meal list component data: {{testString}}</h1>
+  </div>
+  `
+})
+
+export class EditMealComponent {
+  public testString: string = "Data from meal list component is connected";
+}
