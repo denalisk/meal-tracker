d4bd1ff2d86b1dfde47a5ea0769f12b7ec184c61
Sam Kirsch <samdkirsch@gmail.com>
2017-03-31 09:55:13 -0700

    begin filling out the edit meal form

--- app/edit-meal.component.ts
+++ app/edit-meal.component.ts
@@ -4,12 +4,19 @@ import { Meal } from './models/meal.model';
 @Component({
   selector: 'edit-meal',
   template: `
-  <div class="well">
+  <div class="container">
     <h1>edit meal component data: {{testString}}</h1>
+    <div class="edit-meal-form form-group col-sm-6">
+      <input type="text" name="name" [value]="currentMeal.name" (input)="currentMeal.name = $event.target.value" class="form-control">
+      <input type="number" name="calories" [value]="currentMeal.calories" (input)="currentMeal.calories = $event.target.value" class="form-control">
+      <input type="text" name="details" [value]="currentMeal.details" (input)="currentMeal.details = $event.target.value" class="form-control">
+    </div>
   </div>
   `
 })
 
 export class EditMealComponent {
+  @Input() currentMeal: Meal;
+
   public testString: string = "Data from edit meal component is connected";
 }
