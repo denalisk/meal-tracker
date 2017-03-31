291b8a0b3a84e3f2b8e543895685fd376264660c
Sam Kirsch <samdkirsch@gmail.com>
2017-03-31 09:06:42 -0700

    fill out meal list component and check to make sure the data populates on the page

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -4,12 +4,30 @@ import { Meal } from './models/meal.model';
 @Component({
   selector: 'meal-list',
   template: `
-  <div class="well">
+  <div class="container">
     <h1>meal list component data: {{testString}}</h1>
+    <div class="" *ngFor="let currentMeal of history">
+      <div class="col-sm-4">
+        <div class="panel panel-warning">
+          <div class="panel-heading">
+            <h2>{{currentMeal.name}}</h2>
+          </div>
+          <div class="panel-body">
+            <ul>
+              <li>Calories: {{currentMeal.calories}}</li>
+              <li>Notes: {{currentMeal.details}}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
   `
 })
 
 export class MealListComponent {
   public testString: string = "Data from meal list component is connected";
+  public history: Meal[] = Meal.history;
+
+  public
 }
