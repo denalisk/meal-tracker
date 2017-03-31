03c3d3bb46fe55db1d54c6ac906bb6b923362a6b
Sam Kirsch <samdkirsch@gmail.com>
2017-03-31 08:46:03 -0700

    add imports for the thre main components, MealList, NewMeal, and EditMeal

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Meal } from './models/meal.model.ts';
 
+
 @Component({
   selector: 'app-root',
   template: `
   <div class="jumbotron">
     <h1>Hello, World</h1>
   </div>
+  <meal-list></meal-list>
 
   `
 })
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { FormsModule }  from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { MealListComponent } from './meal-list.component';
+import { NewMealComponent } from './new-meal.component';
+import { EditMealComponent } from './edit-meal.component';
+
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, MealListComponent, NewMealComponent, EditMealComponent ],
   bootstrap:    [ AppComponent ]
 })
 
